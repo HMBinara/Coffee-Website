dc9e19d1425ca535ade2696b34051b913c5538dd
H.M.B.S Nethranjana Adhikari <binaranethranganaadikari@gmail.com>
2025-04-11 07:31:59 +0530

    Change
    Adding the menu and testimonials

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 
-menuOpenButton.addEventListener("click", () =>{
+menuOpenButton.addEventListener("click", () => {
     document.body.classList.toggle("show-mobile-menu");
 });
 
-menuCloseButton.addEventListener("click", () =>menuOpenButton.click
-()); 
+menuCloseButton.addEventListener("click", () => menuOpenButton.click
+    ());
+
+const swiper = new Swiper('.slider-wrapper', {
+
+    loop: true,
+
+
+    pagination: {
+        el: '.swiper-pagination',
+    },  
+
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+
+
+});
